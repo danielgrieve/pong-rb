04af767cf7ffa91e33535ba3f4a657e97724d707
Daniel Grieve <daniel.grieve@neo.com>
2013-03-22 18:01:25 +0000

    ball wall collisions

--- lib/pong/ball.rb
+++ lib/pong/ball.rb
@@ -8,20 +8,24 @@ module Pong
       @x = window.width / 2
       @y = 30
 
-      @angle = 0.2 # TODO randomise
-      @speed = 4 # TODO randomise
-      calculate_direction
+      @angle = 45
+      @speed_x = 2 # randomise
+      @speed_y = 2 # randomise
 
       @color = Gosu::Color.new(255, 255, 255, 255)
     end
 
     def move
       if collides_with?(@player)
-        calculate_direction
+        @speed_x = -@speed_x
       end
 
-      @x += @direction_x
-      @y += @direction_y
+      if collides_with_walls?
+        @speed_y = -@speed_y
+      end
+
+      @x += @speed_x
+      @y += @speed_y
     end
 
     def draw
@@ -34,16 +38,21 @@ module Pong
     end
 
     private
-    def calculate_direction
-      @direction_x = @speed * Math.cos(@angle)
-      @direction_y = @speed * Math.sin(@angle)
-    end
-
     def collides_with?(item)
-      new_x = @x + @direction_x
-      new_y = @y + @direction_y
+      new_x = @x + @speed_x
+      new_y = @y + @speed_y
 
       new_x >= item.x && (new_y >= item.y && new_y <= (item.y + item.height))
+    end
+
+    def collides_with_walls?
+      new_y = @y + @speed_y
+
+      new_y >= @window.height || new_y <= 0
+    end
+
+    def deg_to_rad(degrees)
+      degrees * Math::PI / 180
     end
   end
 end
